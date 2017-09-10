7ee9f91f97ea7d82e2428826581740ea98769463
peterccc114 <prettyboy114@mail.com>
2017-09-10 13:59:45 -0400

    CRUD Operations Using HTTp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-
+import { IProduct } from './products';
+import { ProductService } from './products.service';
+//import { appService } from './app.service';
+import { Http , Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';//
 @Component({
   selector: 'my-app',
   templateUrl: `./app.component.html`,
+  providers: [ProductService]
 })
-export class AppComponent  { 
+export class AppComponent  {
+  iproduct: IProduct[];
+  constructor(private _product: ProductService) {
+  } 
+
+  ngOnInit() : void {
+    this._product.getproducts()
+    .subscribe(iproducts => this.iproduct = iproducts);
+  }
   name = 'World1'; 
   appTitle: string = "Welcome";
   appStatus: boolean = true;
@@ -17,4 +31,9 @@ export class AppComponent  {
     "Name": "two"
   }
 ];
+
+
 }
+
+
+
---
+++ src/app/products.ts
@@ -0,0 +1,4 @@
+export interface IProduct {
+    ProductID: number;
+    ProductNme: string; 
+}
